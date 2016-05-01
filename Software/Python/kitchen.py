a909063bdb7ccbde15514bb0678347c9d183856a
= <lukebdecker@gmail.com>
2016-05-01 08:46:18 +0000

    combined motion and rotary scripts

--- Software/Python/kitchen.py
+++ Software/Python/kitchen.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python
 
+from threading import Timer
 import time
 import grovepi
 
-ps = [0, 1, 2]
-ls = [5, 6, 7]
+senzormotion = 4
+litzmotion = 3
+motion=0
+grovepi.pinMode(senzormotion,"INPUT")
+grovepi.pinMode(litzmotion,"OUTPUT")
 
-for p in ps:
-    grovepi.pinMode(p,"INPUT")
-    
-for l in ls:
-    grovepi.pinMode(l,"OUTPUT")
+sensorz = [0, 1, 2]
+lightz = [2, 5, 6]
+for senz in sensorz:
+    grovepi.pinMode(senz,"INPUT")
+for litz in lightz:
+    grovepi.pinMode(litz,"OUTPUT")
 
 time.sleep(1)
 
 adc_ref = 5
 grove_vcc = 5
 full_angle = 300
-prev = [0, 0, 0]
+prevv = [0, 0, 0]
+
+def litzmotionoff():
+    grovepi.digitalWrite(litzmotion,0)
 
 while True:
     try:
-        for i, p in enumerate(ps):
-            sensor_value = grovepi.analogRead(p)
+        # ROTARY LIGHTS
+        for i, senz in enumerate(sensorz):
+            sensor_value = grovepi.analogRead(senz)
             voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
             degrees = round((voltage * full_angle) / grove_vcc, 2)
             brightness = int(degrees / full_angle * 255)
-            if brightness != prev[i]:
-                prev[i] = brightness
-                grovepi.analogWrite(ls[i],brightness)
-            
+            if brightness != prevv[i]:
+                prevv[i] = brightness
+                grovepi.analogWrite(lightz[i],brightness)
+        motion=grovepi.digitalRead(senzormotion)
+        # MOTION LIGHT
+        if motion==0 or motion==1:
+            if motion==1:
+                grovepi.digitalWrite(litzmotion,1)
+                timah = Timer(10,litzmotionoff)
+		timah.start()
     except KeyboardInterrupt:
-        for l in ls:
-            grovepi.analogWrite(l,0)
+        for litz in lightz:
+            grovepi.analogWrite(litz,0)
+        grovepi.digitalWrite(litzmotion,0)
         break
     except IOError:
         print ("Error")
